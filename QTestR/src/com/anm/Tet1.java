1c51aee3c813fae573720361856b7945bb39daa3
anmgroup <48635904+anmgroup@users.noreply.github.com>
2019-03-17 07:21:58 +0530

    Third Update in hub

--- QTestR/src/com/anm/Tet1.java
+++ QTestR/src/com/anm/Tet1.java
@@ -4,7 +4,7 @@ public class Tet1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("AAAAAAAAAAA");
+System.out.println("AAAAtwrtrrwt");
 	}
 
 }
